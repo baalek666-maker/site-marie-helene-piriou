5d538299a2af039df97e2c9810ccbd4cc2357c23
Yo <yo@gruau-lorrain.dev>
2026-06-13 09:10:41 +0200

    Initial commit: site vitrine Marie-Hélène Piriou Psychologue Annecy

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll pour les liens d'ancre
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
